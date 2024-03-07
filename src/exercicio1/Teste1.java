f44e94b40816571ce9b74b7c7b78946d47854b23
JoseERBWolf <wolf.jose89@gmail.com>
2024-03-06 23:32:48 -0300

    Realizando o exercício 1

---
+++ src/exercicio1/Teste1.java
@@ -0,0 +1,29 @@
+package exercicio1;
+
+public class Teste1 {
+    public static void main(String[] args) {
+
+        /*
+        1) Observe o trecho de código abaixo:
+        int INDICE = 13, SOMA = 0, K = 0;
+        enquanto K < INDICE faça
+        {
+        K = K + 1;
+        SOMA = SOMA + K;
+        }
+        imprimir(SOMA);
+        Ao final do processamento, qual será o valor da variável SOMA?
+         */
+
+        int indice = 13;
+        int soma = 0;
+        int k = 0;
+
+        while(k < indice){
+            k+= 1;
+            soma = soma + k;
+        }
+        System.out.println(soma);
+
+    }
+}
